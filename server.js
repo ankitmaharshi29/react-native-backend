24b6d62eafc08968ec6556ba1e55525a7dbe37b5
ankitmaharshi29 <ankitmaharshi55@gmail.com>
2024-09-08 13:36:07 +0530

    Initial commit of backend code

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Import the routes from getdata.js
+const getDataRoutes = require('./getdata');
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Use the routes
+app.use('/api', getDataRoutes);
+
+
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
